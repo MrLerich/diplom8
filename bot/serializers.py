8ac1a3cc8339f80c9e30b29a622c9164e31d8513
MrLerich <93153582+MrLerich@users.noreply.github.com>
2023-01-16 00:25:20 +0300

    init 38 add TOKEN_TG_BOT action.yaml

--- bot/serializers.py
+++ bot/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from bot.models import TgUser
 
@@ -11,10 +12,10 @@ class TgUserSerializer(serializers.ModelSerializer):
         read_only_fields = ('tg_user_id', 'tg_username', 'user_id')
         fields = ('tg_user_id', 'tg_username', 'verification_code', 'user_id')
 
-    # def validate(self, attrs):
-    #     verification_code = attrs.get("verification_code")
-    #     tg_user = TgUser.objects.filter(verification_code=verification_code).first()
-    #     if not tg_user:
-    #         raise ValidationError({"verification_code": "field is incorrect"})
-    #     attrs["tg_user"] = tg_user
-    #     return attrs
+    def validate(self, attrs):
+        verification_code = attrs.get("verification_code")
+        tg_user = TgUser.objects.filter(verification_code=verification_code).first()
+        if not tg_user:
+            raise ValidationError({"verification_code": "field is incorrect"})
+        attrs["tg_user"] = tg_user
+        return attrs
